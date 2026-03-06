73641844d65eade711cfdcd5b8930940ab966835
DominicDev <s0l0estudi4r@gmail.com>
2026-03-06 16:28:43 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'QUEEN LATIFAH';
+export let nameSongs = [
+	"UNITY", /*1*/
+	"LADIES FIRST", /*2*/
+	"U.N.I.T.Y.", /*3*/
+	" JUST ANOTHER DAY", /*4*/
+	"BLACK HAND SIDE", /*5*/
+	"WRATH OF MY MADNESS", /*6*/
+	"COME INTO MY HOUSE", /*7*/
+	"LATIFAH'S HAD IT UP 2 HERE", /*8*/
+	"FLY GIRL", /*9*/
+	"UNITY (REMIX)", /*10*/
+	"MAMMA GAVE BIRTH TO THE SOUL CHILDREN", /*11*/
+	"WEEKEND LOVE", /*12*/
+	"SET IT OFF", /*13*/
+	"NO WORK", /*14*/
+	"I KNOW WHERE I'VE BEEN", /*15*/
+	"RUFFNECK", /*16*/
+	"GO HEAD", /*17*/
+	"PAPER", /*18*/
+	"IF YOU DON'T KNOW", /*19*/
+	"ONE MO' TIME", /*20*/
+]
